903bf045a1e43d049f6422ad8f723b5d5e870b4e
hadijahkyampeire <hadijah315@gmail.com>
2025-03-17 12:27:36 -0500

    Initial project set up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Box, Button, Typography } from '@mui/material'
+import './App.sass'
+
+function App() {
+
+  return (
+    <Box>
+      <Typography>Group one FE</Typography>
+      <Button color='error' variant='outlined'>Testing</Button>
+    </Box>
+  )
+}
+
+export default App
